437994e1884d7a169ef14d0df3ebf67be7892980
roseirungu23 <rosemarywanjikui2016@gmail.com>
2023-04-20 12:09:47 -0700

    6. Size is not grandeur

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,19 +1,11 @@
-#include<stdio.h>
+#include <stdio.h>
+
 int main(void) 
 {
-    char charType;
-    int intType;
-    long int long intType;
-    long long int long long intType;
-    float floatType;
-
-    // sizeof evaluates the size of a variable
-    printf("Size of a char: %d byte\n", sizeof(charType));
-    printf("Size of an int: %d bytes\n", sizeof(intType));
-    printf("Size of a long int: %d bytes\n", sizeof(long intType));
-    printf("Size of a long long int: %d bytes\n", sizeof(long long intType));
-    printf("Size of a float: %d bytes\n", sizeof(floatType));
-    
+    printf("Size of a char: %d byte\n", sizeof(char));
+    printf("Size of an int: %d byte(s)\n", sizeof(int));
+    printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+    printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+    printf("Size of a float: %d byte(s)\n", sizeof(float));
     return 0;
 }
-
