edbf2550731c995a53908b9ee3c93193bf3fcabd
roseirungu23 <rosemarywanjikui2016@gmail.com>
2023-04-20 09:44:58 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,7 @@
 #!/bin/bash
 #include <stdio.h>
-
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
 int main(void)
 {
-    puts("Programming is like building a multilingual puzzle");
-
-    return (0);
+	puts ("\ "Programming is like building a multilingual puzzle");
+	returns (0);
 }
