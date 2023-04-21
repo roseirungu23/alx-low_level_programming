883e876a59ccf33c98c9214d22d8bcb00b54beec
roseirungu23 <rosemarywanjikui2016@gmail.com>
2023-04-20 23:27:54 -0700

    8. UNIX

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,11 @@
+#include <stdio.h>
 #include <unistd.h>
-
-/**main - prints "and that piece of art is useful" -Dora Korpar, 2015-10-19";
- * followed by a newline, to stardard error
- * Return: Always 1
+/**
+ * main - A c program that prints a line to the standard error
+ * Return 1 (success)
  */
-
-
 int main(void)
-
 {
-	write(2, "and that piece of art is useful\" -Dora Kopar, 2015-10-19\n", 59)
-		return (1);
+	fput("and that piece of art is useful\" -Dora Korpar, 2015-10-19\n", stdout);
+	return (1)
 }
