ecd282918923a103d7e53ef0803f0abfbbac3b59
roseirungu23 <rosemarywanjikui2016@gmail.com>
2023-04-24 12:29:30 -0700

    10. Inventing

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,21 +10,21 @@ int main(void)
 {
     int i, j;
 
-    for (i = 0; i < 10; i++)
+    for (i = '0'; i < '9'; i++)
     {
-        for (j = i + 1; j < 10; j++)
-       	{
-            putchar(i / 10 + '0');
-            putchar(i % 10 + '0');
-            putchar(',');
-            putchar(' ');
-            putchar(j / 10 + '0');
-            putchar(j % 10 + '0');
-            if (!(i == 8 && j == 9))
-
-                putchar(' ');
-            }
-        }
+	    for (j = i + 1; j <= '9'; j++)
+	    {
+		    if (j != i)
+		    {
+			    putchar(i);
+			    putchar(j);
+			    if (i == '8' && j == '9')
+				    continue;
+			    putchar(',');
+			    putchar(' ');
+		    }
+	    }
     }
+    putchar('\n');
     return (0);
 }
