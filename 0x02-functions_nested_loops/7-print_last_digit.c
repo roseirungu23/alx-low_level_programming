81428a6db64d86e9f73e3cc4ec38afa70fe7ca82
roseirungu23 <rosemarywanjikui2016@gmail.com>
2023-04-25 09:06:58 -0700

    7. There are only 3

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_last_digit - Prints the last digit of a number
+ * @n: The number to be printed
+ * Return: Value of the last digit of a number
+ */
+int print_last_digit(int n)
+{
+	int ld;
+
+	ld = n % 10;
+	if (ld < 0)
+	{
+	ld = ld * -1;
+	}
+	_putchar(ld + '0');
+	return (ld);
+}
