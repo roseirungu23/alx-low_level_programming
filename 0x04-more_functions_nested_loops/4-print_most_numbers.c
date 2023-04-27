7e84524546bc07cc04dd353be5fd1aaa9bbf4077
roseirungu23 <rosemarywanjikui2016@gmail.com>
2023-04-27 08:48:30 -0700

    4. I believe

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -4,21 +4,22 @@
 /**
  * print_most_numbers - print numbers from 0-9
  * Description: excludes 2 and 4
- * Return: numbers 0-9
+ * Return: numbers 0-9 excluding 2 and 4
  */
 
 void print_most_numbers(void)
 {
+
 	int x;
 
 	for (x = 0; x <= 9; x++)
 	{
-		if (x == 2 && x == 4)
+		if (x == 2 || x == 4)
 		{
 			continue;
 		}
+		else
 			putchar(x + '0');
 	}
 	putchar('\n');
 }
-
