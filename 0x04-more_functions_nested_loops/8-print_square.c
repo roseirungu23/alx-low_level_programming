23fa4de7a88378457695d502ba74cc50090c5747
roseirungu23 <rosemarywanjikui2016@gmail.com>
2023-04-27 11:31:45 -0700

    8. You are so much

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_square - prints a square with a new line
+ * @size: is the size of the square
+ * Return: empty
+ */
+
+void print_square(int size)
+{
+	int x, y;
+
+	if (size <= 0)
+	{
+		putchar('\n');
+	}
+	else
+	{
+		for (x = 0; x < size; x++)
+		{
+			for (y = 0; y < size; y++)
+			{
+				putchar(35);
+			}
+			putchar('\n');
+		}
+	}
+}
