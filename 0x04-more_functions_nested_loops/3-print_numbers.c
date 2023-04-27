5596ce3f1101365ec457cd636789b0c53d2256de
roseirungu23 <rosemarywanjikui2016@gmail.com>
2023-04-27 08:11:41 -0700

    3. The numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -8,11 +8,10 @@
 void print_numbers(void)
 {
 	int x;
-	
-	for (x = 0; x < 10 x++)
+
+	for (x = 0; x <= 9; x++)
 	{
-	_putchar(x + '0');
-	_putchar('\n');
+		_putchar(x + '0');
 	}
 	_putchar('\n');
 }
