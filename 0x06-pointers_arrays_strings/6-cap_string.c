8d40993f3abcd7af85a4ff943b36a571df07d8c1
roseirungu23 <rosemarywanjikui2016@gmail.com>
2023-05-04 13:13:29 -0700

    6. Expect the best

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -13,7 +13,6 @@ char *cap_string(char *str)
 	while (str[i])
 	{
 		while (!(str[i] >= 'a' && str[i] <= 'z'))
-			
 			i++;
 		if (str[i - 1] == ' ' ||
 			str[i - 1] == '\t' ||
@@ -21,15 +20,15 @@ char *cap_string(char *str)
 			str[i - 1] == ',' ||
 			str[i - 1] == ';' ||
 			str[i - 1] == '.' ||
-			str[i - 1] == '!' ||
 			str[i - 1] == '?' ||
+			str[i - 1] == '!' ||
 			str[i - 1] == '"' ||
 			str[i - 1] == '(' ||
 			str[i - 1] == ')' ||
 			str[i - 1] == '{' ||
 			str[i - 1] == '}' ||
 			i == 0)
-				str[i] -= 32;
+			str[i] -= 32;
 		i++;
 	}
 	return (str);
