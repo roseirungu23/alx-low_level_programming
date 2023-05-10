84cfab78098379280e93aebd9f75a37c73d5352f
roseirungu23 <rosemarywanjikui2016@gmail.com>
2023-05-10 07:01:23 -0700

    5. Your subconscious

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+
+int _sqrt_recursion(int n, int i);
+
+/**
+ * _sqrt_recursion -  returns the natural square root of a number
+ * @n: number to get its squareroot
+ *
+ * Return: natural squareroot
+ */
+
+int _sqrt_recursion(int n)
+{
+	return (_sqrt(n, 1));
+}
+
+/**
+ * _sqrt - calculates natural squareroot
+ * @n: number to calculate squareroot
+ * @i: iterate number
+ *
+ * Return: squareroot
+ */
+
+int _sqrt(int n, int i)
+{
+	int sqrt = i * i;
+
+	if (sqrt > n)
+		return (-1);
+	if (sqrt == n)
+		return (i);
+	return (_sqrt(n, i + 1));
+}
