bb76994e3e64f2a356a023bcf1860f0b440d88d6
roseirungu23 <rosemarywanjikui2016@gmail.com>
2023-05-15 14:16:12 -0700

    5. It is not the mountains

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,52 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * argstostr - entry point
+ * @ac: argument count
+ * @av: argument vector
+ *
+ * Return: 0
+ */
+
+char *argstostr(int ac, char **av)
+{
+	int ch = 0;
+	int i = 0;
+	int j = 0;
+	int k = 0;
+	char *s;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	while (i < ac)
+	{
+		while (av[i][j])
+		{
+			ch++;
+			j++;
+		}
+		j = 0;
+		i++;
+	}
+	s = malloc((sizeof(char) * ch) + ac + 1);
+	i = 0;
+	while (av[i])
+	{
+		while (av[i][j])
+		{
+			s[k] = av[i][j];
+			k++;
+			j++;
+		}
+		s[k] = '\n';
+
+		j = 0;
+		k++;
+		i++;
+	}
+	k++;
+	s[k] = '\0';
+	return (s);
+}
