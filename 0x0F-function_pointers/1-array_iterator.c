8267d3129e405a9e72eafc7e070e355c41cf33f7
roseirungu23 <rosemarywanjikui2016@gmail.com>
2023-05-24 09:56:48 -0700

    1. If you spend too much

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,24 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - executes a function given as a parameter
+ * @array: array of elements
+ * @size: size of array
+ * @action: ....
+ *
+ * Retrn: 0
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(init))
+{
+	unsigned int i = 0;
+
+	if (array != NULL && action != NULL && size > 0)
+	{
+		while (i < size)
+		{
+			action(array[i]);
+			i++;
+		}
+	}
+}
