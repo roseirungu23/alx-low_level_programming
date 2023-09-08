deb9e00a796c9320229b9e31866f61c84deca0ca
roseirungu23 <rosemarywanjikui2016@gmail.com>
2023-09-08 07:11:59 -0700

    Hash tables

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,33 @@
+#include "hash_tables.h"
+
+/**
+* hash_table_delete - deletes a hash table
+* @ht: hash table to delete
+*
+* Return: void
+*/
+
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_table_t *head = ht;
+	hash_node_t *node, *tmp;
+	unsigned long int j;
+
+	for (j = 0; j < ht->size; j++)
+	{
+		if (ht->array[j] != NULL)
+		{
+			node = ht->array[j];
+			while (node != NULL)
+			{
+				tmp = node->next;
+				free(node->key);
+				free(node->value);
+				free(node);
+				node = tmp;
+			}
+		}
+	}
+	free(head->array);
+	free(head);
+}
