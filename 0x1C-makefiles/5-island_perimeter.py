88cb44994aaf18991c823ed3f28d35a31f4774ff
roseirungu23 <rosemarywanjikui2016@gmail.com>
2023-10-25 07:52:45 -0700

    Make files

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Defines class island_perimeter"""
+
+
+def island_perimeter(grid):
+    """Calculates the perimeter of an island"""
+    
+    perimeter = 0
+    rows = len(grid)
+    cols = len(grid[0])
+
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 1:
+
+                perimeter += 4
+
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 1
+                if i < rows - 1 and grid[i + 1][j] == 1:
+                    perimeter -= 1
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 1
+                if j < cols - 1 and grid[i][j + 1] == 1:
+                    perimeter -= 1
+    return perimeter
